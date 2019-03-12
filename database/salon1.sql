c31f6b8a42621036e1e5c040afa42d2ac7aa05d0
Ghanshyam <ghanshyam.kumarkahaniya@wabodryms.com>
2019-03-12 15:39:41 +0530

    first commit

--- database/salon1.sql
+++ database/salon1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 08, 2019 at 02:31 PM
+-- Generation Time: Mar 12, 2019 at 11:07 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
